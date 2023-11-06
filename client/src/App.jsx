132808a25fc0275e1c020fa09495d3a6d8b3dcfa
MichelleGoba <goba.michelleolwethu@gmail.com>
2023-11-06 12:52:16 +0200

    added navigation bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,27 @@
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import Chat from "./pages/Chat";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Splashscreen from "./pages/Splashscreen";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import NavigationBar from "./componemts/NavigationBar";
 
 function App() {
-
-
   return (
     <>
-     ChatApp
+      <NavigationBar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Chat />} />
+          <Route path="/rigister" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/splash" element={<Splashscreen />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/Splashscreen.jsx
@@ -0,0 +1,5 @@
+const Splashscreen = () => {
+    return <>Splashscreen</>;
+}
+ 
+export default Splashscreen;
