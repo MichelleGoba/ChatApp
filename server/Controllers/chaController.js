b36b85389a6cc9ab4ec6e7d18f8a75085da86355
MichelleGoba <goba.michelleolwethu@gmail.com>
2023-11-27 15:05:04 +0200

    added findChat function

--- server/Controllers/chaController.js
+++ server/Controllers/chaController.js
@@ -1,22 +1,63 @@
 // createChat
-const chatModel = require ("../Models/chatModel")
-// getUserChats 
+// getUserChats
 // findChat
 
-const createChat = async(req, res) =>{
-    const { firstId, secondId } = req.body
+// createChat
+const chatModel = require("../Models/chatModel");
 
-    try{
-        const chat = await chatModel.findOne({
-            members: {$all: [  firstId, secondId ]}
-        })
-        if(chat) return res.status(200).json(chat);
+const createChat = async (req, res) => {
+  const { firstId, secondId } = req.body;
 
-        const newChat = new chatModel({
-            
-        })
-    }catch(error){
-        console.log(error)
-        res.status(500).json(error)
+  try {
+    const chat = await chatModel.findOne({
+      members: { $all: [firstId, secondId] },
+    });
+    if (chat) return res.status(200).json(chat);
+
+    const newChat = new chatModel({
+      members: [firstId, secondId],
+    });
+
+    // save chat  the database and send it to front end
+    const response = await newChat.save();
+
+    res.staus(200).jsn(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+// findUserChats
+const findUserChats = async (req, res) => {
+  const userId = req.params.userId;
+
+  try {
+    const chats = await chatModel.find({
+      members: { $in: [userId] },
+    });
+
+    // send chat to fron end
+    res.status(200).json(chats);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+// find chat
+const findChat = async (req, res) => {
+    const { firstId, secondId } = req.params
+  
+    try {
+      const chat = await chatModel.find({
+        members: { $all: [firstId, secondId] },
+      });
+  
+      // send chat to fron end
+      res.status(200).json(chat);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
     }
-}
+  };
