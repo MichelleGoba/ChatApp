3e8b9869ce390b18b4011d94a55fe0d0bad65f4f
MichelleGoba <goba.michelleolwethu@gmail.com>
2023-12-08 14:18:36 +0200

    added userChats function

--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -1,5 +1,6 @@
+import { baseUrl, getRequest } from "../utils/services";
 import { createContext, useState, useEffect } from "react";
-import { baseUrl, getRequest, postRequest } from "../utils/services";
+
 
 export const ChatContext = createContext();
 
@@ -7,66 +8,38 @@ export const ChatContextProvider = ({ children, user }) => {
   const [userChats, setUserChats] = useState(null);
   const [isUserChatsLoading, setIsUserChatsLoading] = useState(false);
   const [userChatsError, setUserChatsError] = useState(null);
-  const [potentialChats, setPotentialChats] = useState([])
 
-  useEffect(() => {
-    const getUsers = async() => {
-        const response = await getRequest(`${baseUrl}/users`);
-
-        if(response.error){
-          return console.log("Error fetching users", response)
-        }
-
-        const pChats = response.filter((u) => { 
-          let isChatCreated = false;
-
-          if(user?._id === u._id) return false;
-
-          if(userChats){
-           isChatCreated = userChats?.some((chat)=>{
-              return chat.members[0] === u._id || chat.members[1] === u._id
-            })
-
-          }
-          return !isChatCreated;
-
-        });
-        setPotentialChats(pChats)
-    };
-
-    getUsers()
-  }, [userChats])
-
-  useEffect(() => {
+  useEffect(() =>{
     const getUserChats = async () => {
-      if (user?._id) {
+      if(user?._id){
+
         setIsUserChatsLoading(true);
         setUserChatsError(null);
 
-        const response = await getRequest(`${baseUrl}/chats/${user?._id}`);
+        const response = await getRequest(`${baseUrl}/chats/${user?._id}`)
 
         setIsUserChatsLoading(false);
 
-        if (response.error) {
+        if(response.error){
           return setUserChatsError(response);
         }
 
         setUserChats(response);
       }
-    };
-    getUserChats();
-  }, [user]);
+  }
+  getUserChats()
+  
+}, [user])
 
-  return (
-    <ChatContext.Provider
-      value={{
-        userChats,
-        isUserChatsLoading,
-        userChatsError,
-        potentialChats,
-      }}
-    >
-      {children}
-    </ChatContext.Provider>
-  );
+return (
+  <ChatContext.Provider
+  value={{
+    userChats, 
+    userChatsError,
+    isUserChatsLoading,
+  }}
+  >
+    {children}
+  </ChatContext.Provider>
+);
 };
--- client/src/utils/services.js
+++ client/src/utils/services.js
@@ -35,7 +35,7 @@ export const getRequest = async (url) => {
   const data = await response.json();
 
   if (!response.ok) {
-    let message = "an error occured...";
+    let message = "An error occured...";
 
     if (data?.message) {
       message = data.message;
