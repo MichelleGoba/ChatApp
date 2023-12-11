4f7ae6c5616b6c9d6f5d73d406c618f5cdd5bf6c
MichelleGoba <goba.michelleolwethu@gmail.com>
2023-12-11 14:24:18 +0200

    added list chat function

--- client/src/components/chat/UserChat.jsx
+++ client/src/components/chat/UserChat.jsx
@@ -1,36 +1,36 @@
 import { Container, Stack } from "react-bootstrap";
 import { useFetchRecipientUser } from "../../hooks/useFetchRecipient";
-import "./userChat.css"
+import "./userChat.css";
 
 const UserChat = ({ chat, user }) => {
   const { recipientUser } = useFetchRecipientUser(chat, user);
-  console.log(recipientUser);
 
   return (
-    <Container >
-    <Stack
-      direction="horizontal"
-      gap={3}
-      className="user-card align-items-center"
-      p-2
-      justify-content-between
-    >
+ 
+     <Container >
+     <Stack
+       direction="horizontal"
+       gap={3}
+       className="user-card align-items-center"
+       p-2="true"
+       justify-content-between="true"
+     >
       <div className="d-flex">
-        <div className="me-2">A</div>
-        <div className="text-content">
-          <div className="name">{recipientUser?.username}</div>
-          <div className="text">Text Message</div>
-        </div>
-      </div>
-      <div className="d-flex flex-column align-items-end">
-        <div className="date">
-            12/12/2022
-        </div>
-        <div className="this-user-notifications">2</div>
-        <span className="user-online"></span>
-      </div>
-    </Stack>
-    </Container>
+         <div className="me-2">image</div>
+         <div className="text-content">
+           <div className="name">{recipientUser?.username}</div>
+           <div className="text">Text Message</div>
+         </div>
+       </div>
+       <div className="d-flex flex-column align-items-end">
+         <div className="date">
+             12/12/2022
+         </div>
+         <div className="this-user-notifications">2</div>
+         <span className="user-online"></span>
+       </div>
+     </Stack>
+     </Container>
   );
 };
 
--- client/src/hooks/useFetchRecipient.js
+++ client/src/hooks/useFetchRecipient.js
@@ -1,33 +1,26 @@
-// this is the other user
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { baseUrl, getRequest } from "../utils/services";
 
 export const useFetchRecipientUser = (chat, user) => {
-    const [recipientUser, setRecipientUser] = useState(null)
-    const [error, setError] = useState(null) 
+  const [recipientUser, setRecipientUser] = useState(null);
+  const [error, setError] = useState(null);
 
-    const recipientId = chat?.members.find((id) => id !==user?._id)
+  const recipientId = chat?.members.find((id) => id !== user?._id);
+  
 
-    console.log("chat", chat);
+  useEffect(() => {
+    const getUser = async () => {
+      if (!recipientId) return null;
 
-    useEffect(() =>{
-        const getUser = async() =>{
+      const response = await getRequest(`${baseUrl}/users/find/${recipientId}`);
+      if (response.error) {
+        return setError(response);
+      }
 
-            if(!recipientId) return null
+      setRecipientUser(response);
+    };
+    getUser();
+  }, []);
 
-            const response = await getRequest(`${baseUrl}/users/find/${recipientId}`)
-
-            if(response.error){
-                return setError(response)
-            }
-
-            setRecipientUser(response)
-        }
-        getUser()
-    }, []);
-
-    return {recipientUser}
-
+  return { recipientUser };
 };
-
-
