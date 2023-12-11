607f0e36093c268dea68bfee894c2d1a55bea0e2
MichelleGoba <goba.michelleolwethu@gmail.com>
2023-12-11 11:24:45 +0200

    added router to get messages using the chat Id

--- server/Routes/messageRoute.js
+++ server/Routes/messageRoute.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const { getMessages } = require("../Controllers/messageController");
 
 const router = express.Router();
 
 router.post("/", createMessage);
+router.get("/:chatid", getMessages);
 
 
 module.exports = router;
