4d6034a0d401a16fce36de9d9da91b60392dd070
MichelleGoba <goba.michelleolwethu@gmail.com>
2024-01-24 12:13:56 +0200

    added footer message on the chats

--- client/src/components/chat/ChatBox.jsx
+++ client/src/components/chat/ChatBox.jsx
@@ -3,28 +3,43 @@ import { AuthContext } from "../../context/AuthContext";
 import { ChatContext } from "../../context/ChatContext";
 import { useFetchRecipientUser } from "../../hooks/useFetchRecipient";
 import { Stack } from "react-bootstrap";
+import moment from "moment";
 
 const ChatBox = () => {
   const { user } = useContext(AuthContext);
   const { currentChat, messages, isMessagesLoading } = useContext(ChatContext);
-  const { reciepientUser } = useFetchRecipientUser(currentChat, user);
+  const { recipientUser } = useFetchRecipientUser(currentChat, user);
 
-  if(!reciepientUser) return(
-    <p style={{textAlign: "center", width:"100%" }}>No conversation selected</p>
+  if (!recipientUser)
+    return (
+      <p style={{ textAlign: "center", width: "100%", color: "red" }}>
+        No conversation selected...
+      </p>
+    );
+
+  if (isMessagesLoading)
+    return (
+      <p style={{ textAlign: "center", width: "100%", color: "red" }}>
+        Loading Chat...
+      </p>
+    );
+
+  return (
+    <Stack gap={4} className="chat-box">
+      <div className="chat-header">
+        <strong style={{ color: "white" }}>{recipientUser?.username}</strong>
+      </div>
+      <Stack>
+        {messages &&
+          messages.map((message, index) => (
+            <Stack key={index} className={`$(message?.senderId === user?._id)`}>
+              <span>{message.text}</span>
+              <span className="message-footer">{moment(message.createdAt).calendar()}</span>
+            </Stack>
+          ))}
+      </Stack>
+    </Stack>
   );
-  if(isMessagesLoading) return(
-    <p style={{textAlign: "center", width:"100%" }}>Loading chat</p>
-  );
-
-
-  return <Stack gap={4} className="chat-box">
-    <div className="chat-header">
-        <strong>{reciepientUser?.username}</strong>
-        </div>Chat Box</Stack>;
-
-  
 };
-
-
 
 export default ChatBox;
